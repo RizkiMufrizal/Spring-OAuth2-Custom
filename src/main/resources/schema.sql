78b983865a444761ef444ae098ef679b19381a98
RizkiMufrizal <mufrizalrizki@gmail.com>
2017-01-30 18:49:26 +0700

    tambah schema database

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,14 @@
+drop table if exists oauth_client_details;
+create table oauth_client_details (
+  client_id VARCHAR(255) PRIMARY KEY,
+  resource_ids VARCHAR(255),
+  client_secret VARCHAR(255),
+  scope VARCHAR(255),
+  authorized_grant_types VARCHAR(255),
+  web_server_redirect_uri VARCHAR(255),
+  authorities VARCHAR(255),
+  access_token_validity INTEGER,
+  refresh_token_validity INTEGER,
+  additional_information VARCHAR(4096),
+  autoapprove VARCHAR(255)
+);
