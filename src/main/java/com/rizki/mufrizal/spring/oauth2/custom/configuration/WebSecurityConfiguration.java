43b190f702d268248b71ee7e7eb630ff5f7be877
RizkiMufrizal <mufrizalrizki@gmail.com>
2017-01-30 18:50:26 +0700

    tambah konfigurasi spring security

---
+++ src/main/java/com/rizki/mufrizal/spring/oauth2/custom/configuration/WebSecurityConfiguration.java
@@ -0,0 +1,42 @@
+package com.rizki.mufrizal.spring.oauth2.custom.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ *
+ * @Author Rizki Mufrizal <mufrizalrizki@gmail.com>
+ * @Web <https://RizkiMufrizal.github.io>
+ * @Since Jan 30, 2017
+ * @Time 1:17:05 PM
+ * @Encoding UTF-8
+ * @Project Spring-OAuth2-Custom
+ * @Package com.rizki.mufrizal.spring.oauth2.custom.configuration
+ *
+ */
+@Configuration
+@EnableWebSecurity(debug = true)
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
+        authenticationManagerBuilder
+                .inMemoryAuthentication()
+                .withUser("admin")
+                .password("admin")
+                .roles("ADMIN");
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+}
