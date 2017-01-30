af8b4dc9bc3bbfd69d072dfe6aace18ac9f0a79c
RizkiMufrizal <mufrizalrizki@gmail.com>
2017-01-30 10:25:23 +0700

    initial project

---
+++ src/main/java/com/rizki/mufrizal/spring/oauth2/custom/SpringOAuth2CustomApplication.java
@@ -0,0 +1,12 @@
+package com.rizki.mufrizal.spring.oauth2.custom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOAuth2CustomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringOAuth2CustomApplication.class, args);
+	}
+}
---
+++ src/test/java/com/rizki/mufrizal/spring/oauth2/custom/SpringOAuth2CustomApplicationTests.java
@@ -0,0 +1,16 @@
+package com.rizki.mufrizal.spring.oauth2.custom;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringOAuth2CustomApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
