cc4f0e6c69b74a90cea2b40dbac2eb7a052240e9
RizkiMufrizal <mufrizalrizki@gmail.com>
2017-01-30 18:50:10 +0700

    tambah konfigurasi datasource

---
+++ src/main/java/com/rizki/mufrizal/spring/oauth2/custom/configuration/DataSourceConfiguration.java
@@ -0,0 +1,74 @@
+package com.rizki.mufrizal.spring.oauth2.custom.configuration;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.jdbc.datasource.init.DatabasePopulator;
+import javax.sql.DataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.core.io.Resource;
+import org.springframework.jdbc.datasource.init.DataSourceInitializer;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
+
+/**
+ *
+ * @Author Rizki Mufrizal <mufrizalrizki@gmail.com>
+ * @Web <https://RizkiMufrizal.github.io>
+ * @Since Jan 30, 2017
+ * @Time 12:49:36 PM
+ * @Encoding UTF-8
+ * @Project Spring-OAuth2-Custom
+ * @Package com.rizki.mufrizal.spring.oauth2.custom.configuration
+ *
+ */
+@Configuration
+@PropertySource("classpath:application.yml")
+public class DataSourceConfiguration {
+
+    @Autowired
+    private Environment environment;
+
+    @Value("classpath:schema.sql")
+    private Resource schemaScript;
+
+    @Value("classpath:data.sql")
+    private Resource dataScript;
+
+    @Bean(destroyMethod = "close")
+    public DataSource dataSource() {
+        HikariConfig dataSourceConfig = new HikariConfig();
+        dataSourceConfig.setDriverClassName(environment.getRequiredProperty("spring.datasource.driver-class-name"));
+        dataSourceConfig.setJdbcUrl(environment.getRequiredProperty("spring.datasource.url"));
+        dataSourceConfig.setUsername(environment.getRequiredProperty("spring.datasource.username"));
+        dataSourceConfig.setPassword(environment.getRequiredProperty("spring.datasource.password"));
+        dataSourceConfig.setMaximumPoolSize(Integer.parseInt(environment.getRequiredProperty("spring.datasource.maximumPoolSize")));
+        dataSourceConfig.setMinimumIdle(Integer.parseInt(environment.getRequiredProperty("spring.datasource.minimumIdle")));
+        dataSourceConfig.setMaxLifetime(Integer.parseInt(environment.getRequiredProperty("spring.datasource.maxLifetime")));
+        dataSourceConfig.setConnectionTimeout(Long.parseLong(environment.getRequiredProperty("spring.datasource.connectionTimeout")));
+        dataSourceConfig.setIdleTimeout(Long.parseLong(environment.getRequiredProperty("spring.datasource.idleTimeout")));
+        dataSourceConfig.addDataSourceProperty("poolName", environment.getRequiredProperty("spring.datasource.poolName"));
+        dataSourceConfig.addDataSourceProperty("cachePrepStmts", "true");
+        dataSourceConfig.addDataSourceProperty("prepStmtCacheSize", "250");
+        dataSourceConfig.addDataSourceProperty("prepStmtCacheSqlLimit", "2048");
+        return new HikariDataSource(dataSourceConfig);
+    }
+
+    private DatabasePopulator databasePopulator() {
+        final ResourceDatabasePopulator resourceDatabasePopulator = new ResourceDatabasePopulator();
+        resourceDatabasePopulator.addScript(schemaScript);
+        resourceDatabasePopulator.addScript(dataScript);
+        return resourceDatabasePopulator;
+    }
+
+    @Bean
+    public DataSourceInitializer dataSourceInitializer(final DataSource dataSource) {
+        final DataSourceInitializer initializer = new DataSourceInitializer();
+        initializer.setDataSource(dataSource);
+        initializer.setDatabasePopulator(databasePopulator());
+        return initializer;
+    }
+}
