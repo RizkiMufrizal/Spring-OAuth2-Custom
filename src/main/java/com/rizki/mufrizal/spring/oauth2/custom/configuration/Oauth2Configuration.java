db9fb028a710953b20684643702f3306880493c8
RizkiMufrizal <mufrizalrizki@gmail.com>
2017-02-05 22:38:42 +0700

    update OAuth2 configuration

--- src/main/java/com/rizki/mufrizal/spring/oauth2/custom/configuration/Oauth2Configuration.java
+++ src/main/java/com/rizki/mufrizal/spring/oauth2/custom/configuration/Oauth2Configuration.java
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
@@ -15,9 +14,9 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
-import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
 import org.springframework.security.oauth2.provider.token.TokenStore;
-import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
+import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.web.access.channel.ChannelProcessingFilter;
 
 /**
@@ -63,8 +62,6 @@ public class Oauth2Configuration {
     @EnableAuthorizationServer
     protected static class AuthorizationServerConfiguration extends AuthorizationServerConfigurerAdapter {
 
-        private final TokenStore tokenStore = new InMemoryTokenStore();
-
         @Autowired
         @Qualifier("authenticationManagerBean")
         private AuthenticationManager authenticationManager;
@@ -72,10 +69,16 @@ public class Oauth2Configuration {
         @Autowired
         private DataSource dataSource;
 
+        @Bean
+        public JwtAccessTokenConverter jwtAccessTokenConverter() {
+            return new JwtAccessTokenConverter();
+        }
+
         @Override
         public void configure(AuthorizationServerEndpointsConfigurer authorizationServerEndpointsConfigurer) throws Exception {
             authorizationServerEndpointsConfigurer
-                    .tokenStore(this.tokenStore)
+                    .accessTokenConverter(jwtAccessTokenConverter())
+                    .tokenStore(tokenStore())
                     .authenticationManager(this.authenticationManager);
         }
 
@@ -86,12 +89,8 @@ public class Oauth2Configuration {
         }
 
         @Bean
-        @Primary
-        public DefaultTokenServices tokenServices() {
-            final DefaultTokenServices defaultTokenServices = new DefaultTokenServices();
-            defaultTokenServices.setTokenStore(this.tokenStore);
-            defaultTokenServices.setSupportRefreshToken(true);
-            return defaultTokenServices;
+        public TokenStore tokenStore() {
+            return new JdbcTokenStore(dataSource);
         }
 
     }
