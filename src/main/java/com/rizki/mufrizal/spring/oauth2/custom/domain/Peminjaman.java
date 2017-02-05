4ba74b301fbcf95da2a3dc498e82020a8cae2c4c
RizkiMufrizal <mufrizalrizki@gmail.com>
2017-02-05 22:35:24 +0700

    add domain peminjaman

--- src/main/java/com/rizki/mufrizal/spring/oauth2/custom/domain/Peminjaman.java
+++ src/main/java/com/rizki/mufrizal/spring/oauth2/custom/domain/Peminjaman.java
@@ -2,10 +2,14 @@ package com.rizki.mufrizal.spring.oauth2.custom.domain;
 
 import java.io.Serializable;
 import java.util.Date;
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
@@ -39,4 +43,82 @@ public class Peminjaman implements Serializable {
     @Temporal(TemporalType.DATE)
     @Column(name = "tanggal_batas_pengembalian")
     private Date tanggalBatasPengembalian;
+
+    @ManyToOne(cascade = {CascadeType.ALL}, fetch = FetchType.LAZY)
+    @JoinColumn(name = "npm", insertable = false, updatable = false, nullable = false)
+    private Mahasiswa mahasiswa;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_buku")
+    private Buku buku;
+
+    /**
+     * @return the idPeminjaman
+     */
+    public String getIdPeminjaman() {
+        return idPeminjaman;
+    }
+
+    /**
+     * @param idPeminjaman the idPeminjaman to set
+     */
+    public void setIdPeminjaman(String idPeminjaman) {
+        this.idPeminjaman = idPeminjaman;
+    }
+
+    /**
+     * @return the tanggalPeminjaman
+     */
+    public Date getTanggalPeminjaman() {
+        return tanggalPeminjaman;
+    }
+
+    /**
+     * @param tanggalPeminjaman the tanggalPeminjaman to set
+     */
+    public void setTanggalPeminjaman(Date tanggalPeminjaman) {
+        this.tanggalPeminjaman = tanggalPeminjaman;
+    }
+
+    /**
+     * @return the tanggalBatasPengembalian
+     */
+    public Date getTanggalBatasPengembalian() {
+        return tanggalBatasPengembalian;
+    }
+
+    /**
+     * @param tanggalBatasPengembalian the tanggalBatasPengembalian to set
+     */
+    public void setTanggalBatasPengembalian(Date tanggalBatasPengembalian) {
+        this.tanggalBatasPengembalian = tanggalBatasPengembalian;
+    }
+
+    /**
+     * @return the mahasiswa
+     */
+    public Mahasiswa getMahasiswa() {
+        return mahasiswa;
+    }
+
+    /**
+     * @param mahasiswa the mahasiswa to set
+     */
+    public void setMahasiswa(Mahasiswa mahasiswa) {
+        this.mahasiswa = mahasiswa;
+    }
+
+    /**
+     * @return the buku
+     */
+    public Buku getBuku() {
+        return buku;
+    }
+
+    /**
+     * @param buku the buku to set
+     */
+    public void setBuku(Buku buku) {
+        this.buku = buku;
+    }
 }
