41273bfe1eab9ab461d0624b832cdfbe7eb99276
RizkiMufrizal <mufrizalrizki@gmail.com>
2017-02-05 22:35:31 +0700

    add domain mahasiswa

--- src/main/java/com/rizki/mufrizal/spring/oauth2/custom/domain/Mahasiswa.java
+++ src/main/java/com/rizki/mufrizal/spring/oauth2/custom/domain/Mahasiswa.java
@@ -1,12 +1,17 @@
 package com.rizki.mufrizal.spring.oauth2.custom.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
 import javax.persistence.Id;
 import javax.persistence.Lob;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 /**
@@ -41,4 +46,91 @@ public class Mahasiswa implements Serializable {
     @Lob
     @Column(name = "alamat")
     private String alamat;
+
+    @OneToMany(cascade = {CascadeType.ALL}, orphanRemoval = true, fetch = FetchType.LAZY, mappedBy = "mahasiswa")
+    private List<Peminjaman> peminjamans = new ArrayList<>();
+
+    /**
+     * @return the npm
+     */
+    public String getNpm() {
+        return npm;
+    }
+
+    /**
+     * @param npm the npm to set
+     */
+    public void setNpm(String npm) {
+        this.npm = npm;
+    }
+
+    /**
+     * @return the nama
+     */
+    public String getNama() {
+        return nama;
+    }
+
+    /**
+     * @param nama the nama to set
+     */
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    /**
+     * @return the kelas
+     */
+    public String getKelas() {
+        return kelas;
+    }
+
+    /**
+     * @param kelas the kelas to set
+     */
+    public void setKelas(String kelas) {
+        this.kelas = kelas;
+    }
+
+    /**
+     * @return the jenisKelamin
+     */
+    public JenisKelamin getJenisKelamin() {
+        return jenisKelamin;
+    }
+
+    /**
+     * @param jenisKelamin the jenisKelamin to set
+     */
+    public void setJenisKelamin(JenisKelamin jenisKelamin) {
+        this.jenisKelamin = jenisKelamin;
+    }
+
+    /**
+     * @return the alamat
+     */
+    public String getAlamat() {
+        return alamat;
+    }
+
+    /**
+     * @param alamat the alamat to set
+     */
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
+
+    /**
+     * @return the peminjamans
+     */
+    public List<Peminjaman> getPeminjamans() {
+        return peminjamans;
+    }
+
+    /**
+     * @param peminjamans the peminjamans to set
+     */
+    public void setPeminjamans(List<Peminjaman> peminjamans) {
+        this.peminjamans = peminjamans;
+    }
 }
