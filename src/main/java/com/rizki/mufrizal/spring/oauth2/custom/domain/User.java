0af28427f01ebb27c3d9564465db09bd4375942c
RizkiMufrizal <mufrizalrizki@gmail.com>
2017-02-05 22:35:12 +0700

    add domain user

--- src/main/java/com/rizki/mufrizal/spring/oauth2/custom/domain/User.java
+++ src/main/java/com/rizki/mufrizal/spring/oauth2/custom/domain/User.java
@@ -1,5 +1,16 @@
 package com.rizki.mufrizal.spring.oauth2.custom.domain;
 
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
 /**
  *
  * @Author Rizki Mufrizal <mufrizalrizki@gmail.com>
@@ -11,6 +22,76 @@ package com.rizki.mufrizal.spring.oauth2.custom.domain;
  * @Package com.rizki.mufrizal.spring.oauth2.custom.domain
  *
  */
-public class User {
+@Entity
+@Table(name = "tb_user")
+public class User implements Serializable {
 
+    @Id
+    @Column(name = "username", length = 50, nullable = false)
+    private String username;
+
+    @Column(name = "password", length = 100, nullable = false)
+    private String password;
+
+    @Column(name = "is_active", nullable = false)
+    private Boolean isActive;
+
+    @OneToMany(cascade = {CascadeType.ALL}, fetch = FetchType.LAZY, mappedBy = "user")
+    private Set<UserRole> userRoles = new HashSet<>();
+
+    /**
+     * @return the username
+     */
+    public String getUsername() {
+        return username;
+    }
+
+    /**
+     * @param username the username to set
+     */
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    /**
+     * @return the password
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * @param password the password to set
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    /**
+     * @return the isActive
+     */
+    public Boolean getIsActive() {
+        return isActive;
+    }
+
+    /**
+     * @param isActive the isActive to set
+     */
+    public void setIsActive(Boolean isActive) {
+        this.isActive = isActive;
+    }
+
+    /**
+     * @return the userRoles
+     */
+    public Set<UserRole> getUserRoles() {
+        return userRoles;
+    }
+
+    /**
+     * @param userRoles the userRoles to set
+     */
+    public void setUserRoles(Set<UserRole> userRoles) {
+        this.userRoles = userRoles;
+    }
 }
