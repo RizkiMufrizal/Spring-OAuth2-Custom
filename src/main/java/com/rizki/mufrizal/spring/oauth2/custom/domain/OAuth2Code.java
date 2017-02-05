e2e71ac34bbf5db2830154b5f38fb2e380d785e1
RizkiMufrizal <mufrizalrizki@gmail.com>
2017-02-05 22:34:37 +0700

    add domain OAuth2 code

---
+++ src/main/java/com/rizki/mufrizal/spring/oauth2/custom/domain/OAuth2Code.java
@@ -0,0 +1,78 @@
+package com.rizki.mufrizal.spring.oauth2.custom.domain;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ *
+ * @Author Rizki Mufrizal <mufrizalrizki@gmail.com>
+ * @Web <https://RizkiMufrizal.github.io>
+ * @Since Feb 5, 2017
+ * @Time 7:12:25 PM
+ * @Encoding UTF-8
+ * @Project Spring-OAuth2-Custom
+ * @Package com.rizki.mufrizal.spring.oauth2.custom.domain
+ *
+ */
+@Entity
+@Table(name = "oauth_code")
+public class OAuth2Code implements Serializable {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    @Column(name = "code")
+    private String code;
+
+    @Column(name = "authentication")
+    private byte[] authentication;
+
+    /**
+     * @return the id
+     */
+    public Integer getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the code
+     */
+    public String getCode() {
+        return code;
+    }
+
+    /**
+     * @param code the code to set
+     */
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    /**
+     * @return the authentication
+     */
+    public byte[] getAuthentication() {
+        return authentication;
+    }
+
+    /**
+     * @param authentication the authentication to set
+     */
+    public void setAuthentication(byte[] authentication) {
+        this.authentication = authentication;
+    }
+}
