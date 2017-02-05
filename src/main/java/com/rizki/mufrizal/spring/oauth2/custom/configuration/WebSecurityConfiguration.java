cacd84273d597d9840167e122779723c22986328
RizkiMufrizal <mufrizalrizki@gmail.com>
2017-02-05 22:40:51 +0700

    add web security configuration

--- src/main/java/com/rizki/mufrizal/spring/oauth2/custom/configuration/WebSecurityConfiguration.java
+++ src/main/java/com/rizki/mufrizal/spring/oauth2/custom/configuration/WebSecurityConfiguration.java
@@ -1,5 +1,7 @@
 package com.rizki.mufrizal.spring.oauth2.custom.configuration;
 
+import com.rizki.mufrizal.spring.oauth2.custom.service.impl.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -7,6 +9,8 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 /**
  *
@@ -24,13 +28,19 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    private UserService userService;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
     @Override
     protected void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
         authenticationManagerBuilder
-                .inMemoryAuthentication()
-                .withUser("admin")
-                .password("admin")
-                .roles("ADMIN");
+                .userDetailsService(userService)
+                .passwordEncoder(passwordEncoder());
     }
 
     @Bean
