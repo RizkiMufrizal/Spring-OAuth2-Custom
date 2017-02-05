770b61094028cdd028f559bb8894fd77a8c0e12b
RizkiMufrizal <mufrizalrizki@gmail.com>
2017-02-05 22:34:56 +0700

    add enum role

---
+++ src/main/java/com/rizki/mufrizal/spring/oauth2/custom/domain/Role.java
@@ -0,0 +1,16 @@
+package com.rizki.mufrizal.spring.oauth2.custom.domain;
+
+/**
+ *
+ * @Author Rizki Mufrizal <mufrizalrizki@gmail.com>
+ * @Web <https://RizkiMufrizal.github.io>
+ * @Since Feb 5, 2017
+ * @Time 6:18:37 PM
+ * @Encoding UTF-8
+ * @Project Spring-OAuth2-Custom
+ * @Package com.rizki.mufrizal.spring.oauth2.custom.domain
+ *
+ */
+public enum Role {
+    ROLE_ADMIN, ROLE_USER, ROLE_CLIENT
+}
