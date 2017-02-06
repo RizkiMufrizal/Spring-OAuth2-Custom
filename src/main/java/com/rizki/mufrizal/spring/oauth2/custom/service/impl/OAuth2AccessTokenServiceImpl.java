7741a8bedb0dee0821a98831dfdb34b135794143
RizkiMufrizal <mufrizalrizki@gmail.com>
2017-02-06 21:11:51 +0700

    add service OAuth2 access token impl

---
+++ src/main/java/com/rizki/mufrizal/spring/oauth2/custom/service/impl/OAuth2AccessTokenServiceImpl.java
@@ -0,0 +1,33 @@
+package com.rizki.mufrizal.spring.oauth2.custom.service.impl;
+
+import com.rizki.mufrizal.spring.oauth2.custom.domain.OAuth2AccessToken;
+import com.rizki.mufrizal.spring.oauth2.custom.repository.OAuth2AccessTokenRepository;
+import com.rizki.mufrizal.spring.oauth2.custom.service.OAuth2AccessTokenService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ *
+ * @Author Rizki Mufrizal <mufrizalrizki@gmail.com>
+ * @Web <https://RizkiMufrizal.github.io>
+ * @Since Feb 6, 2017
+ * @Time 9:04:46 PM
+ * @Encoding UTF-8
+ * @Project Spring-OAuth2-Custom
+ * @Package com.rizki.mufrizal.spring.oauth2.custom.service.impl
+ *
+ */
+@Service
+@Transactional(readOnly = true)
+public class OAuth2AccessTokenServiceImpl implements OAuth2AccessTokenService {
+
+    @Autowired
+    private OAuth2AccessTokenRepository oAuth2AccessTokenRepository;
+
+    @Override
+    public OAuth2AccessToken findByClientId(String clientId) {
+        return oAuth2AccessTokenRepository.findByClientId(clientId);
+    }
+
+}
