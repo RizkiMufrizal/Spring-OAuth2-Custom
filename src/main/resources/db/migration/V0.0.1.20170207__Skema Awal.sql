0ac20a7a0d5a2d223b653b39fa40b5d317f98440
RizkiMufrizal <mufrizalrizki@gmail.com>
2017-02-07 17:49:01 +0700

    tambah konfigurasi flyway

---
+++ src/main/resources/db/migration/V0.0.1.20170207__Skema Awal.sql
@@ -0,0 +1,11 @@
+/**
+ *
+ * @Author Rizki Mufrizal <mufrizalrizki@gmail.com>
+ * @Web <https://RizkiMufrizal.github.io>
+ * @Since Feb 07, 2017
+ * @Time 10:27:33 AM
+ * @Encoding UTF-8
+ * @Project Spring-OAuth2-Custom
+ * @Package 
+ *
+ */
